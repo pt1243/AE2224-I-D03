2001763a364d6c3779d023ed6f81ac7272409894
M. Pluciński <124085105+mplucinski2@users.noreply.github.com>
2023-03-16 16:02:39 +0100

    abc

--- test_read.py
+++ test_read.py
@@ -1,4 +1,9 @@
-with open('data/L2_S2/L2_S2_cycle_0/C1_mono_1_saine_emetteur_1_recepteur_2_excitation_5_00000.trc', 'rb') as f:
-    contents = f.readlines()
+#import npz files stored in the same directory
+import numpy as np
 
-print(contents)
+# Load npz file
+data = np.load('L2_S2_cycle_0\C1_mono_1_saine_emetteur_1_recepteur_2_excitation_5_00000.npz')
+print(data)
+# Access the data in the npz file
+array1 = data['arr_0']
+array2 = data['arr_1']
