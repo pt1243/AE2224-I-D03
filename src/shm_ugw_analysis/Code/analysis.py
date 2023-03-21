62cf62da1f9b977fc919a972e924c98d3bd73e6c
0northlake0 <r20dux@gmail.com>
2023-03-21 12:21:23 +0100

    analysis

--- src/shm_ugw_analysis/Code/analysis.py
+++ src/shm_ugw_analysis/Code/analysis.py
@@ -1,26 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from shm_ugw_analysis.data_io.load import load_data
-import di_fnct as di
+import di_functions as di
 
 
-cycles = ['0', '1', '1000', '10000', '20000', '30000', '40000', '50000', '60000', '70000']
+cycles = ['1', '1000', '10000', '20000', '30000', '40000', '50000', '60000', '70000']
 frequencies = list(range(100, 200, 20))
 
 
-def data(cycle='0', emitter=1, receiver=4, frequency=100):
-    x, t, desc = load_data(cycle=cycle, signal_type='received', emitter=emitter, receiver=receiver, frequency=frequency)
-    start, end = np.searchsorted(t, -0.5e-5), np.searchsorted(t, 2.5e-5)
-    result = x[start:end]
-    result = (result - np.mean(result)/np.std(result))
-    return result
-
-
 def plot_di():
-    dis = np.array([di.spatial_phase_difference(cycle=c, emitter=1, receiver=5) for c in cycles])
+    dis = np.array([di.central_spectrum_loss(cycle=c, emitter=1, receiver=4) for c in cycles])
     dis -= np.min(dis)
     dis /= np.max(dis)
-    plt.plot(cycles, dis)
+    plt.plot(list(map(int, cycles)), dis)
     plt.show()
     pass
 
---
+++ src/shm_ugw_analysis/Code/di_functions.py
@@ -0,0 +1,53 @@
+import numpy as np
+from shm_ugw_analysis.data_io.load import load_data
+
+
+def data(cycle='0', emitter=1, receiver=4, frequency=100):
+    x, t, desc = load_data(cycle=cycle, signal_type='received', emitter=emitter, receiver=receiver, frequency=frequency)
+    start, end = np.searchsorted(t, -0.5e-5), np.searchsorted(t, 2.5e-5)
+    result = x[start:end]
+    result = (result - np.mean(result)/np.std(result))
+    return result
+
+
+def cross_correlation(cycle='70000', emitter=1, receiver=4, frequency=100):
+    x0 = data(cycle='0', emitter=emitter, receiver=receiver, frequency=frequency)
+    x = data(cycle=cycle, emitter=emitter, receiver=receiver, frequency=frequency)
+    return 1 - np.sqrt((np.sum(x0*x) ** 2) / (np.sum(x0*x0) * np.sum(x*x)))
+    pass
+
+
+def spatial_phase_difference(cycle='70000', emitter=1, receiver=4, frequency=100):
+    x0 = data(cycle='0', emitter=emitter, receiver=receiver, frequency=frequency)
+    x = data(cycle=cycle, emitter=emitter, receiver=receiver, frequency=frequency)
+    d = x / np.sqrt(np.sum(x*x))
+    a = np.sum(d*x0) / np.sum(x0*x0)
+    return np.sum((d-a*x0)**2)
+
+
+def spectrum_loss(cycle='70000', emitter=1, receiver=4, frequency=100):
+    x0 = np.fft.fft(data(cycle='0', emitter=emitter, receiver=receiver, frequency=frequency))
+    x = np.fft.fft(data(cycle=cycle, emitter=emitter, receiver=receiver, frequency=frequency))
+    return np.sum(np.abs(x0-x)) / np.sum(np.abs(x0))
+
+
+def central_spectrum_loss(cycle='70000', emitter=1, receiver=4, frequency=100):
+    x0 = np.fft.fft(data(cycle='0', emitter=emitter, receiver=receiver, frequency=frequency))
+    x = np.fft.fft(data(cycle=cycle, emitter=emitter, receiver=receiver, frequency=frequency))
+    a, b = np.max(x0), np.max(x)
+    return (a-b)/a
+
+
+def differential_curve_energy(cycle='70000', emitter=1, receiver=4, frequency=100):
+    x0 = data(cycle='0', emitter=emitter, receiver=receiver, frequency=frequency)
+    x = data(cycle=cycle, emitter=emitter, receiver=receiver, frequency=frequency)
+    b = x0 - x
+    return np.sum((b[1:] - b[:-1])**2) / np.sum((x0[1:] - x0[:-1])**2)
+
+
+def differential_signal_energy(cycle='70000', emitter=1, receiver=4, frequency=100):
+    x0 = data(cycle='0', emitter=emitter, receiver=receiver, frequency=frequency)
+    x = data(cycle=cycle, emitter=emitter, receiver=receiver, frequency=frequency)
+    b = x0 / np.sqrt(np.sum(x0 * x0))
+    d = x / np.sqrt(np.sum(x*x))
+    return np.sum((b-d)**2)
