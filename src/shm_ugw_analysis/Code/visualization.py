e594ee9506276f4f37f38a429a470790e3b93ca5
0northlake0 <r20dux@gmail.com>
2023-03-21 10:01:21 +0100

    visualization

--- src/shm_ugw_analysis/Code/visualization.py
+++ src/shm_ugw_analysis/Code/visualization.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from shm_ugw_analysis.data_io.load import load_data
 
-x, t, desc = load_data(cycle='0', signal_type='received', emitter=2, receiver=5, frequency=100)
+x, t, desc = load_data(cycle='70000', signal_type='received', emitter=1, receiver=4, frequency=100)
 
-print(np.shape(x), np.shape(t), desc)
-plt.plot(x[2475:2625], t[2475:2625])
+start, end = np.searchsorted(t, -0.5e-5), np.searchsorted(t, 2.5e-5)
+plt.plot(t[start:end], x[start:end])
 plt.show()
