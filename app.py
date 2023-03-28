edbe4e3724a68a2143ab8e249dcc542b3dd01c2d
M. Pluciński <124085105+mplucinski2@users.noreply.github.com>
2023-03-28 08:49:36 +0200

    abc

--- app.py
+++ app.py
@@ -34,41 +34,6 @@ for j in range(0, len(frequency)):
 #write code loading data into numpy array
 t = datasetfinal[1,:, 0, 0, 0]
 x = datasetfinal[0, :, 0, 0, 0]
-
-t_begin = t[0]
-t_end = t[-1]
-deltat = t[1]-t[0]
-samplingfreq = 1/(t[1]-t[0])
-plt.plot(t, x)
-
-
-mean = np.mean(x)
-std = np.std(x)
-x_std = (x-mean)/std
-skewness = sp.stats.skew(x_std)
-kurtosis = sp.stats.kurtosis(x_std)
-crest_factor = np.max(x)/np.sqrt(np.mean(x**2))
-k_factor = np.max(x)/np.mean(x)
-
-
-
-freq, Psd = signal.welch(x_std, samplingfreq, nperseg=1024)
-
-x_fft = np.fft.fft(x_std)
-
-Mag = np.abs(x_fft)
-Phase = np.angle(x_fft)
-
-frequencies = np.fft.fftfreq(len(x_std), 1/samplingfreq)
-
-
-#delete all zero values in numpy array
-window_size = 3
-order = 1
-
-x_savgol=savgol_filter(x_std, window_size, order)
-i=10
-
 dir_path = os.path.join(os.getcwd(), 'plots')
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
@@ -81,23 +46,26 @@ for h in range(0, len(frequency)):
                 
                 x = datasetfinal[2*j, :, k, z, h]
                 t = datasetfinal[2*j+1, :, k, z, h]
+                samplingfreq = 1/(t[1]-t[0])
                 x_std = (x - np.mean(x))/np.std(x)
                 x_fft = np.fft.fft(x_std)
                 #apply gaussian filter
                 x_fft = sp.ndimage.gaussian_filter1d(x_fft, sigma=10)
                 #numerical integration of fft using trapezoid rule
                 Mag = np.abs(x_fft)
-                Mag = Mag/np.trapz(Mag, dx = 1/samplingfreq)
+                
                 frequencies = np.fft.fftfreq(len(x_std), 1/samplingfreq)
+                #integrate fft wrt frequency
+                Mag=Mag/np.trapz(Mag, dx = np.max(frequencies)/len(frequencies))
                 ax1.plot(frequencies, Mag, label = 'cycle '+str(cycles[j]))
-                ax1.grid()
+                ax1.grid()  
                 ax1.set_xlabel('Frequency [Hz]')
                 ax1.set_ylabel('Amplitude')
                 ax1.set_xlim(0.5*10**5, 7*10**5)
                 ax1.legend()
                 ax1.set_title('FFT'+ 'receiver'+str(receiver[k]) + 'emitter'+str(emitter[z])+'frequency'+str(frequency[h])+'KHz')
                 freq, Psd = signal.welch(x_std, samplingfreq, nperseg=1024)
-                PSD = Psd/np.trapz(Psd, dx = 1/samplingfreq)
+                PSD = Psd/np.trapz(Psd, dx = freq[-1]/len(freq))
                 #set plot size
                 ax2.plot(freq, Psd, label = 'cycle '+str(cycles[j]))
                 ax2.grid()
