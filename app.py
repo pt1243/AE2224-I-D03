f36e126938a41b1d981ab90bff38e6d6c36a0843
M. Pluciński <124085105+mplucinski2@users.noreply.github.com>
2023-03-28 13:03:42 +0200

    final

--- app.py
+++ app.py
@@ -34,60 +34,64 @@ for j in range(0, len(frequency)):
         for z in range(0, len(emitter)):
             for k in range(0 , len(receiver)):
             #create two subplots
-            
+                for a in range(0, 2):
+                    if a==1:
+                        s = Signal(str(cycles[h]), 'excitation', receiver[k], emitter[z], frequency[j])
+                    else:
+                        s = Signal(str(cycles[h]), 'excitation', emitter[z], receiver[k], frequency[j])
                 #plot two subplots
-                s = Signal(str(cycles[h]), 'excitation', emitter[z], receiver[k], frequency[j])
-                t = s.t
-                x = s.x
-                samplingfreq = 1/(t[1]-t[0])
-                x_std = (x - np.mean(x))/np.std(x)
-                x_fft = np.fft.fft(x_std)
-                Mag = np.abs(x_fft)
-                frequencies = np.fft.fftfreq(len(x_std), 1/samplingfreq)
-                #apply gaussian filter
-                sort = np.argsort(frequencies)
-                frequencies = frequencies[sort]
-                Mag = Mag[sort]
-                Mag = sp.ndimage.gaussian_filter1d(Mag, sigma=15)
-                def maxr(freq, Mag):
-                    max = 0
-                    index = 0
-                    for i in range(0, len(Mag)):
-                        if freq[i] > (frequency[j]-20)*10**3 and freq[i] < (frequency[j]+20)*10**3:
-                            if Mag[i] > max:
-                                max = Mag[i]
-                                index = i
-                    return max, index
-
-                #numerical integration of fft using trapezoid rule
-                
-                #compute power spectral density (PSD ) of x_std
-                freq, psd = signal.welch(x_std, samplingfreq, nperseg = 1024, scaling = 'spectrum')
-               
-                #sort frequencies and corresponding fft values
-
-                #integrate fft wrt frequency
-                Mag=Mag/np.trapz(Mag, dx = np.max(frequencies)/len(frequencies))
-                #function evaluating polynomial with coefficients polycfft 
-
                     
+                    t = s.t
+                    x = s.x
+                    samplingfreq = 1/(t[1]-t[0])
+                    x_std = (x - np.mean(x))/np.std(x)
+                    x_fft = np.fft.fft(x_std)
+                    Mag = np.abs(x_fft)
+                    frequencies = np.fft.fftfreq(len(x_std), 1/samplingfreq)
+                    #apply gaussian filter
+                    sort = np.argsort(frequencies)
+                    frequencies = frequencies[sort]
+                    Mag = Mag[sort]
+                    Mag = sp.ndimage.gaussian_filter1d(Mag, sigma=15)
+                    def maxr(freq, Mag):
+                        max = 0
+                        index = 0
+                        for i in range(0, len(Mag)):
+                            if freq[i] > (frequency[j]-20)*10**3 and freq[i] < (frequency[j]+20)*10**3:
+                                if Mag[i] > max:
+                                    max = Mag[i]
+                                    index = i
+                        return max, index
 
-                #find index of the maximum value
-                #plot the point on the graph
+                    #numerical integration of fft using trapezoid rule
+                    
+                    #compute power spectral density (PSD ) of x_std
+                    freq, psd = signal.welch(x_std, samplingfreq, nperseg = 1024, scaling = 'spectrum')
                 
-            
+                    #sort frequencies and corresponding fft values
+
+                    #integrate fft wrt frequency
+                    Mag=Mag/np.trapz(Mag, dx = np.max(frequencies)/len(frequencies))
+                    #function evaluating polynomial with coefficients polycfft 
+
+                        
+
+                    #find index of the maximum value
+                    #plot the point on the graph
+                    
                 
-                
-                sort = np.argsort(freq)
-                freq = freq[sort]
-                psd = psd[sort]
-                psd = psd/np.trapz(psd, dx = np.max(freq)/len(freq))
-                max, index = maxr(frequencies, Mag)
-                Maxmag.append(max)
-                Maxfrequency.append(frequencies[index])
-                max, index = maxr(freq, psd)
-                Maxpsd.append(max)
-                Maxfreq.append(freq[index])
+                    
+                    
+                    sort = np.argsort(freq)
+                    freq = freq[sort]
+                    psd = psd[sort]
+                    psd = psd/np.trapz(psd, dx = np.max(freq)/len(freq))
+                    max, index = maxr(frequencies, Mag)
+                    Maxmag.append(max)
+                    Maxfrequency.append(frequencies[index])
+                    max, index = maxr(freq, psd)
+                    Maxpsd.append(max)
+                    Maxfreq.append(freq[index])
     #sort freq and corresponding PSD values
         ax1.scatter(Maxfrequency , Maxmag,   label = 'cycle '+str(cycles[h]), color = colors[h])
         ax2.scatter(Maxfreq, Maxpsd, label = 'cycle '+str(cycles[h]), color = colors[h])
@@ -100,13 +104,14 @@ for j in range(0, len(frequency)):
     ax2.set_xlabel('Frequency [Hz]')
     ax2.set_ylabel('Power (W)')
     #ax2.set_xlim((frequency[j]-20)*10**3, (frequency[j]+20)*10**3)
-    ax2.legend()
+    #position legend to the right of the subplots
+    ax2.legend(loc = 'upper right')
     ax2.set_title('PSD')
     ax1.grid()  
     ax1.set_xlabel('Frequency [Hz]')
     ax1.set_ylabel('Amplitude')
     file_path = os.path.join(dir_path, 'FFT+PSD'+'emitter'+str(emitter[z])+'receiver'+str(receiver[k])+ 'frequency'+str(frequency[j])+'KHz'+'.png')
-    plt.savefig(file_path, dpi = 500)
+    plt.savefig(file_path, dpi = 400)
     plt.clf()
     
            
