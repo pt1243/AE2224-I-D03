3aa80db78e78d03b1b221ca23c5d276470e9ac9a
michieleland <m.p.w.eland@student.tudelft.nl>
2023-03-28 09:19:42 +0200

    Update test.py

--- src/shm_ugw_analysis/test/test.py
+++ src/shm_ugw_analysis/test/test.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.fft import fft, ifft
 import matplotlib.pyplot as plt 
-from data_io.load import load_data
+from shm_ugw_analysis.data_io.load import load_data
 
 # Load npz file
 data = np.load('L2_S2_cycle_0\C1_mono_1_saine_emetteur_1_recepteur_2_excitation_5_00000.npz')
