a33efefff556271ac3039579016ec01467d58f37
thibault <95294291+t-clara@users.noreply.github.com>
2023-03-28 20:38:01 +0200

    Cleaning up FFT&PSD

--- src/shm_ugw_analysis/stats_processing/welch.py
+++ src/shm_ugw_analysis/stats_processing/welch.py
@@ -1,76 +1,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from scipy import signal
+from scipy.ndimage import gaussian_filter1d
 from scipy.signal import savgol_filter, welch
+from scipy import ndimage
 import os
 import pandas as pd
 from shm_ugw_analysis.data_io.load import load_data, allowed_emitters, allowed_receivers, allowed_frequencies
 from shm_ugw_analysis.data_io.signal import Signal, signal_collection
 from shm_ugw_analysis.data_io.paths import ROOT_DIR
 import pathlib
+from numpy.fft import fft, fftfreq, fftshift
+from matplotlib.ticker import ScalarFormatter
 
 
 PLOT_DIR = ROOT_DIR.joinpath('plots')
 if not pathlib.Path.exists(PLOT_DIR):
     pathlib.Path.mkdir(PLOT_DIR)
 
+# Note that Welch's method is a cross power spectral density approximation Pxy 
+# although it would be applied with respect to itself in the context of a 1D array.
+
+# We may apply scipy.signal.coherence to create a unitless comparison of similarity
+# between 2 time series vectors x and y computed using Welch cross power spectral
+# density equations: Cxy = abs(Pxy)**2/(Pxx*Pyy)
+
 def fft_and_psd_plots(sc: signal_collection, bin_width, emitter, receiver):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
     for s in sc:
+        # Signal Segmentation
         s: Signal
         x = s.x
         t = s.t
         # Parameters
         fs = s.sample_frequency
+        #print(f'fs={fs}')
         nperseg = int(fs/bin_width)
         # Normalizing
         x_std = (x - np.mean(x))/np.std(x)
 
         # Standard FFT Method
-        #x_fft = endaq.calc.fft.aggregate_fft(x_std)
+        ## Option 1: Hanning Window
+        '''
+        N = len(x)
+        window = np.hanning(N)
+        x_filtered = x_std * window
+        y_fft = np.fft.fft(x_filtered)/N
+        x_fft = np.fft.fftfreq(N, d=1/fs)
+        y_fft_magnitude = np.abs(y_fft)
+        y_fft_magnitude = gaussian_filter1d(y_fft_magnitude, sigma=10)
+        #y_fft_magnitude_dB = 10*np.log10(y_fft_magnitude)
+        '''
+        ## Option 2: 
+        N = len(x)
+        y_fft = fft(x_std)
+        x_fft = fftfreq(N, d=1/fs)
+        y_fft_magnitude = np.abs(y_fft)
+        y_fft_magnitude = gaussian_filter1d(y_fft_magnitude, sigma=5)
+        #y_fft_magnitude_dB = 10*np.log10(y_fft_magnitude)
         
-        # Welch FFT Method
-        x_fft_welch, y_fft_welch = welch(x, fs=fs, nperseg=nperseg)
-        #x_fft = np.fft.fft(x_std)
+        # Welch PSD Method
+        x_psd_welch, y_psd_welch = welch(x, fs, nperseg=nperseg)
+        y_psd_welch_magnitude = np.abs(y_psd_welch)
+        y_psd_welch_magnitude_dB = 10*np.log10(y_psd_welch_magnitude)
 
-        # Gaussian Filter
-        #x_fft = sp.ndimage.gaussian_filter1d(x_fft_welch, sigma=10)
-        
-        # numerical integration of fft using trapezoid rule
-        Mag = np.abs(y_fft_welch)
-        frequencies = x_fft_welch
-        #frequencies = np.fft.fftfreq(len(x_std), 1/samplingfreq)
-        #integrate fft wrt frequency
-        # Mag=Mag/np.trapz(Mag, dx = np.max(frequencies)/len(frequencies))
-        ax1.plot(frequencies, Mag, label = f'cycle {s.cycle}')
-
-        freq, Psd = signal.welch(x_std, fs, nperseg=nperseg)
-        ax2.plot(freq, Psd, label = f'cycle {s.cycle}')
+        # Plotting
+        ax1.plot(x_fft, y_fft_magnitude, label = f'cycle {s.cycle}')
+        ax2.plot(x_psd_welch, y_psd_welch_magnitude_dB, label = f'cycle {s.cycle}')
 
     ax1.grid()  
     ax1.set_xlabel('Frequency [Hz]')
     ax1.set_ylabel('Amplitude')
     ax1.set_xlim(0.5*10**5, 7*10**5)
+    ax1.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+    ax1.ticklabel_format(style="sci", axis="x", scilimits=(0,0))
     ax1.legend()
-    # ax1.set_title('FFT'+ 'receiver'+str(receiver[k]) + 'emitter'+str(emitter[z])+'frequency'+str(frequency[h])+'KHz')
-    ax1.set_title(f'FFT emitter {emitter} receiver {receiver} frequency {s.frequency} kHz')
-    
-    # PSD = Psd/np.trapz(Psd, dx = freq[-1]/len(freq))
-    #set plot size
-    
+    ax1.set_title(f'FFT emitter {emitter} receiver {receiver} frequency {s.frequency} kHz')    
     ax2.grid()
     ax2.set_xlabel('Frequency [Hz]')
     ax2.set_ylabel('Power (W)')
     ax2.set_xlim(0.5*10**5, 7*10**5)
+    ax2.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+    ax2.ticklabel_format(style="sci", axis="x", scilimits=(0,0))
     ax2.legend()
-    # ax2.set_title('PSD'+ 'receiver'+str(receiver[k]) + 'emitter'+str(emitter[z])+'frequency'+str(frequency[h])+'KHz')
     ax2.set_title(f'PSD emitter {emitter} receiver {receiver} frequency {s.frequency} kHz')
-    # file_path = os.path.join(dir_path, 'FFT+PSD'+'emitter'+str(emitter[z])+'receiver'+str(receiver[k])+ 'frequency'+str(frequency[h])+'KHz'+'.png')
     file_path = os.path.join(PLOT_DIR, f'FFT+PSD_emitter_{emitter}_receiver_{receiver}_frequency_{s.frequency}_kHz.png')
     plt.savefig(file_path, dpi = 500)
     return
-
 
 cycles=['0', '1', '10000', '20000', '30000', '40000', '50000', '60000', '70000']
 # cycles = ['0']
@@ -87,11 +103,9 @@ for frequency in frequencies:
         for receiver in receivers:
             try:
                 sc = signal_collection(cycles=tuple(cycles), signal_types=tuple(signal_types), emitters=[emitter], receivers=[receiver], frequencies=[frequency])
-                fft_and_psd_plots(sc, 1000, emitter, receiver)
+                fft_and_psd_plots(sc, 10000, emitter, receiver)
             except Exception:
                 continue
-        
-
 
 # for s in sc:
 #     fft_and_psd_plots(s, 5000)
