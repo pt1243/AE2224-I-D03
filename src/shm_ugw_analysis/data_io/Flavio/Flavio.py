3edfcc68b0336c7d64cdcce68f2ef0c9b556dd6a
Flavio Claudio Padua <flaviopadua22@gmail.com>
2023-03-28 09:13:58 +0200

    Update Flavio.py

--- src/shm_ugw_analysis/data_io/Flavio/Flavio.py
+++ src/shm_ugw_analysis/data_io/Flavio/Flavio.py
@@ -24,50 +24,129 @@ def plot(cycle: str, signal_type: str, emitter: int, receiver: int, frequency: i
     plt.ylim(y_bounds[0],y_bounds[1])
     plt.xlabel('t')
     plt.ylabel('y')
-    plt.legend()
+    plt.legend(fontsize=5)
     plt.savefig('test.png')
     return y,t,desc,frequency
 
 cycle = '0' 
-signal_type = 'excitation'
-emitter = 1
+signal_type = 'received'
+emitter = 2
 receiver = 4
 frequency = 100
 
 x_bounds = [0,0.00002] #x domain shown
 y_bounds = [-10,10] #y domain shown
+x_bounds1 = [0,0.00002]
+y_bounds1 = [-0.25,0.25]
 
 def r_vs_em(cycle: str, emitter: int, receiver:int, frequency: int, x_bounds: list, y_bounds: list):
     plot(cycle, 'excitation', emitter, receiver, frequency, x_bounds, y_bounds)
     plot(cycle, 'received', emitter, receiver, frequency, x_bounds, y_bounds)
 
-r_vs_em(cycle, emitter, receiver, frequency, x_bounds, y_bounds)
+def cycle_comparison(emitter: int, receiver:int, frequency: int, x_bounds: list, y_bounds: list):
+    plot('0', 'received', emitter, receiver, frequency, x_bounds, y_bounds)
+    plot('1', 'received', emitter, receiver, frequency, x_bounds, y_bounds)
+    plot('1000', 'received', emitter, receiver, frequency, x_bounds, y_bounds)
+    plot('10000', 'received', emitter, receiver, frequency, x_bounds, y_bounds)
+    plot('20000', 'received', emitter, receiver, frequency, x_bounds, y_bounds)
+    plot('30000', 'received', emitter, receiver, frequency, x_bounds, y_bounds)
+    plot('40000', 'received', emitter, receiver, frequency, x_bounds, y_bounds)
+    plot('50000', 'received', emitter, receiver, frequency, x_bounds, y_bounds)
+    plot('60000', 'received', emitter, receiver, frequency, x_bounds, y_bounds)
+    plot('70000', 'received', emitter, receiver, frequency, x_bounds, y_bounds)
 
-y, t, desc = load_data(cycle, signal_type, emitter, receiver, frequency)
+def freq_comparison(cycle: str, emitter: int, receiver:int, x_bounds: list, y_bounds: list):
+    plot(cycle, 'received', emitter, receiver, 100, x_bounds, y_bounds)
+    plot(cycle, 'received', emitter, receiver, 120, x_bounds, y_bounds)
+    plot(cycle, 'received', emitter, receiver, 140, x_bounds, y_bounds)
+    plot(cycle, 'received', emitter, receiver, 160, x_bounds, y_bounds)
+    plot(cycle, 'received', emitter, receiver, 180, x_bounds, y_bounds)
 
-graph = np.array([t,y])
-scales = np.arange(y_bounds[0], y_bounds[1]+1)
-tstep = desc[0] # time step
-Fs = desc[1] # sampling freq
-f0 = frequency # signal freq
 
-coef, freqs = pywt.cwt(y, scales, 'gaus1') #Finding CWT using Gaussian wavelet
+def scalogram_plot(cycle: str, signal_type: str, emitter: int, receiver:int, frequency: int):
 
-#Scalogram plot
-plt.figure(figsize=(15,10))
-plt.imshow(abs(coef), extent=[x_bounds[0], x_bounds[1], y_bounds[1], y_bounds[0]], interpolation='bilinear', cmap='bone', aspect='auto', vmax=abs(coef).max(), vmin=-abs(coef).max())
-plt.gca().invert_yaxis()
-plt.yticks(np.arange(y_bounds[0],y_bounds[1]+1,1))
-plt.xticks(np.arange(x_bounds[0],x_bounds[1]+1,0.00001))
-plt.savefig('Scalogram.png')
+    y, t, desc = load_data(cycle, signal_type, emitter, receiver, frequency)
+
+    scales = np.arange(1, 31)
+
+    coef, freqs = pywt.cwt(y, scales, 'morl') #Finding CWT
+
+    fig = plt.figure(figsize=(15,10))
+    plt.imshow(abs(coef), extent=[0, 200, 30, 1], interpolation='bilinear', cmap='hot', aspect='auto', vmax=abs(coef).max(), vmin=-abs(coef).max())
+    #plt.gca().invert_yaxis()
+    plt.yticks(np.arange(1, 31, 1))
+    plt.xticks(np.arange(0, 201, 1))
+    label = "cycle_" + str(cycle) + "-" + str(signal_type) + "-emitter_" + str(emitter) + "-receiver_" + str(receiver) + "-frequency_" + str(frequency)
+    plt.title(label, fontsize=25)
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    plt.xlim(19,22)
+    plt.colorbar()
+    plt.savefig('Scalogram.png')
+    return abs(coef)
+
+def scalogram_comparison(cycle: str, frequency: int):
+
+    fig, scalogram = plt.subplots(3, 3)
+
+    scalogram[0,0].plot(scalogram_plot(cycle, 'received', 1, 4, frequency))
+    scalogram[0,1].plot(scalogram_plot(cycle, 'received', 2, 4, frequency))
+    scalogram[0,2].plot(scalogram_plot(cycle, 'received', 3, 4, frequency))
+    scalogram[1,0].plot(scalogram_plot(cycle, 'received', 1, 5, frequency))
+    scalogram[1,1].plot(scalogram_plot(cycle, 'received', 2, 5, frequency))
+    scalogram[1,2].plot(scalogram_plot(cycle, 'received', 3, 5, frequency))
+    scalogram[2,0].plot(scalogram_plot(cycle, 'received', 1, 6, frequency))
+    scalogram[2,1].plot(scalogram_plot(cycle, 'received', 2, 6, frequency))
+    scalogram[2,2].plot(scalogram_plot(cycle, 'received', 3, 6, frequency))
+
+    scalogram[0,0].set_xlim(-19,21)
+    scalogram[0,0].set_ylim(1,30)
+
+    plt.tight_layout()
+    plt.savefig('Scalogram_comp.png')
+
+
+#scalogram_plot(cycle, signal_type, emitter, receiver, frequency)
+scalogram_comparison(cycle, frequency)
+
+
+#scalogram_plot('0', 'received', 1, 4, 100)
+#freq_comparison(cycle, emitter, receiver, x_bounds1, y_bounds1)
+#cycle_comparison(emitter, receiver, frequency, x_bounds1, y_bounds1)
+#r_vs_em(cycle, emitter, receiver, frequency, x_bounds, y_bounds)
+
+#y, t, desc = load_data(cycle, signal_type, emitter, receiver, frequency)
+
+#graph = np.array([t,y])
+#scales = np.arange(1, 31)
+#tstep = desc[0] # time step
+#Fs = desc[1] # sampling freq
+#f0 = frequency # signal freq
+
+#coef, freqs = pywt.cwt(y, scales, 'morl') #Finding CWT
+
+
+#SCALOGRAM PLOT
+
+#plt.figure(figsize=(15,10))
+#plt.imshow(abs(coef), extent=[0, 200, 30, 1], interpolation='bilinear', cmap='hot', aspect='auto', vmax=abs(coef).max(), vmin=-abs(coef).max())
+#plt.gca().invert_yaxis()
+#plt.yticks(np.arange(1, 31, 1))
+#plt.xticks(np.arange(0, 201, 1))
+#label = "cycle_" + str(cycle) + "-" + str(signal_type) + "-emitter_" + str(emitter) + "-receiver_" + str(receiver) + "-frequency_" + str(frequency)
+#plt.title(label, fontsize=25)
+#plt.xlabel('Time')
+#plt.ylabel('Frequency')
+#plt.xlim(19,22)
+#plt.savefig('Scalogram.png')
 
 #Signal plot
-plt.figure(figsize=(15, 10))
-plt.plot(t,y)
-plt.xlim(x_bounds[0],x_bounds[1])
-plt.ylim(y_bounds[0],y_bounds[1])
-plt.grid(color='gray', linestyle=':', linewidth=0.5)
-plt.savefig('signal_plot.png')
+#plt.figure(figsize=(15, 10))
+#plt.plot(t,y)
+#plt.xlim(x_bounds[0],x_bounds[1])
+#plt.ylim(y_bounds[0],y_bounds[1])
+#plt.grid(color='gray', linestyle=':', linewidth=0.5)
+#plt.savefig('signal_plot.png')
 
 
 
