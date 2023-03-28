4c9741ac124c4729104ac24a772c7ad42a6c8a1d
michieleland <m.p.w.eland@student.tudelft.nl>
2023-03-28 09:16:20 +0200

    Update test.py

--- src/shm_ugw_analysis/test/test.py
+++ src/shm_ugw_analysis/test/test.py
@@ -1,8 +1,14 @@
 import numpy as np
 from scipy.fft import fft, ifft
 import matplotlib.pyplot as plt 
-from shm_ugw_analysis.data_io.load import load_data
+from data_io.load import load_data
 
+# Load npz file
+data = np.load('L2_S2_cycle_0\C1_mono_1_saine_emetteur_1_recepteur_2_excitation_5_00000.npz')
+print(data)
+# Access the data in the npz file
+array1 = data['arr_0']
+array2 = data['arr_1']
 
 x, t, desc = load_data(
     cycle='0',
@@ -11,5 +17,6 @@ x, t, desc = load_data(
     receiver=4,
     frequency=100
 )
-print(np.shape(x), np.shape(t), desc)
-plt.plot(x[2475:2625], t[2475:2625])
+
+print(x)
+
