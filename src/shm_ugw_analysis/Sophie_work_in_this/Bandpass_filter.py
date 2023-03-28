bf084d23ee38e4270cff8a2fadd6ddd79d8a6e53
SophieLoogman <scmloogman@gmail.com>
2023-03-28 10:19:29 +0200

    Update Bandpass_filter.py

--- src/shm_ugw_analysis/Sophie_work_in_this/Bandpass_filter.py
+++ src/shm_ugw_analysis/Sophie_work_in_this/Bandpass_filter.py
@@ -14,12 +14,14 @@ x_1, t_1, desc_1 = load_data(
 )
 
 #for now applied on a rondom signal, with added noise, should be added to signal
-t = np.linspace(-1, 1, 201)
-x = (np.sin(2*np.pi*0.75*t*(1-t) + 2.1) +
-     0.1*np.sin(2*np.pi*1.25*t + 1) +
-     0.18*np.cos(2*np.pi*3.85*t))
-rng = np.random.default_rng()
-x_noisy = x + rng.standard_normal(len(t)) * 0.08
+#t = np.linspace(-1, 1, 201)
+t = t_1
+x_noisy = x_1
+#x = (np.sin(2*np.pi*0.75*t*(1-t) + 2.1) +
+#0.1*np.sin(2*np.pi*1.25*t + 1) +
+    # 0.18*np.cos(2*np.pi*3.85*t))
+#rng = np.random.default_rng()
+#x_noisy = x + rng.standard_normal(len(t)) * 0.08
 
 #second order, lowpass filter, in which b,a are both 1D arrays
 b, a = signal.butter(3, 0.05, btype='low') 
@@ -32,7 +34,7 @@ z2, _ = signal.lfilter(b, a, z, zi=zi*z[0])
 #now we apply the filter by using filfilt
 y = signal.filtfilt(b, a, x_noisy)
 
-plt.figure()
+plt.figure
 plt.plot(t, x_noisy, 'b', alpha=0.75)
 plt.plot(t, z, 'r--', t, z2, 'r', t, y, 'k')
 plt.legend(('noisy signal', 'lfilter, once', 'lfilter, twice',
