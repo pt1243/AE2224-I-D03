0651a4504874fc0ae004ea946cefeff80f69b6f7
sdkalpoe <s.d.kalpoe@student.tudelft.nl>
2023-05-04 15:03:50 +0200

    ggg

--- src/shm_ugw_analysis/Code/visualization.py
+++ src/shm_ugw_analysis/Code/visualization.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from shm_ugw_analysis.data_io.load import load_data
+from shm_ugw_analysis.data_io.signal import Signal
 
-x, t, desc = load_data(cycle='1', signal_type='received', emitter=1, receiver=6, frequency=100)
 
-start, end = np.searchsorted(t, -0.5e-5), np.searchsorted(t, 2.5e-5)
+s = Signal('10000', 'received', 2, 5, 180)
+x, t = s.x, s.t
+
+start, end = np.searchsorted(t, -0.5e-5), np.searchsorted(t, 5e-5)
 plt.plot(t[start:end], x[start:end])
+#plt.plot(t, x)
 plt.show()
