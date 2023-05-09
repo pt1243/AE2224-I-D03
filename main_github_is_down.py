8689113f4a12ea6cede7b4d1722434f60e5a83f6
thibault <95294291+t-clara@users.noreply.github.com>
2023-05-09 17:34:15 +0200

    envelope and plot troubleshooting

--- main_github_is_down.py
+++ main_github_is_down.py
@@ -71,13 +71,16 @@ for cycle in relevant_cycles:
 
 for cycle in relevant_cycles:
     fig, ax = plt.subplots(1, 1, figsize=(12, 10))
-    for frequency in allowed_frequencies:
-        sc = signal_collection(cycles=(cycle,), signal_types=('received',), frequencies=(frequency,), emitters=allowed_emitters, receivers=allowed_receivers, residual=True)
+    # col = 25001
+    envelope_matrix = np.empty([len(allowed_frequencies), 12501])
+    for row_index, frequency in enumerate(allowed_frequencies):
+        sc = signal_collection(cycles=(cycle,), signal_types=('received',), frequencies=(frequency,), emitters=allowed_emitters, receivers=allowed_receivers, residual=False)
         for i, s in enumerate(sc):
             fs = s.sample_frequency
             buttered_array = butter_lowpass(s.x, fs, order=20)
             #buttered_array = s.x
             buttered_fft = our_fft(buttered_array, fs, sigma=20)
+            #print(len(buttered_fft), len(s.x))
             if i == 0:
                 average_buttered_fft = buttered_fft
             else:
@@ -85,14 +88,26 @@ for cycle in relevant_cycles:
         average_buttered_fft /= (i + 1)
         x_peaks, y_peaks = real_find_peaks(average_buttered_fft)
         x_min, y_min = real_find_peaks((average_buttered_fft[0], -average_buttered_fft[1]))
-        ax.plot(x_peaks, y_peaks, "x")
-        ax.plot(x_min, -y_min, "x")
-        ax.plot(average_buttered_fft[0], average_buttered_fft[1], label=f'buttered cycle {cycle}, received, all paths, {frequency} kHz')
+        #ax.plot(x_peaks, y_peaks, "x")
+        #ax.plot(x_min, -y_min, "x")
+        #ax.plot(average_buttered_fft[0], average_buttered_fft[1], label=f'buttered cycle {cycle}, received, all paths, {frequency} kHz')
         #unbuttered_fft = our_fft(s.x, fs)
         #plt.plot(unbuttered_fft[0], unbuttered_fft[1], label=f'unbuttered Cycle {s.cycle}, {s.signal_type}, {s.emitter}-{s.receiver}, {s.frequency}')
+        envelope_matrix[row_index, :] = average_buttered_fft[1] #.flatten()
+    upper_envelope = np.max(envelope_matrix, axis=0)
+    print(envelope_matrix)
+    print(upper_envelope)
+    lower_envelope = np.min(envelope_matrix, axis=0)
+    ax.plot(average_buttered_fft[0], upper_envelope)
+    ax.plot(average_buttered_fft[0], lower_envelope)
+    ax.fill_between(average_buttered_fft[0], lower_envelope, upper_envelope)
+    #print(average_buttered_fft.shape)
     ax.legend()
+    ax.set_title(f'FFT: Buttered cycle {cycle}, received, all paths (averaged), all allowed_frequencies, residual=False')
+    ax.set_xlabel('Frequency [Hz]')
+    ax.set_ylabel('Magnitude in dB[-]')
     ax.set_xlim(0, 450000)
-    file_path = os.path.join(PLOT_DIR, f'buttered cycle {cycle}, received, all paths (averaged), all allowed_frequencies, residual=True')
+    file_path = os.path.join(PLOT_DIR, f'buttered cycle {cycle}, received, all paths (averaged), all allowed_frequencies, residual=False')
     plt.savefig(file_path, dpi = 500)
-    #plt.show()
+    plt.show()
     plt.clf()
